9e6180a6f1718c8d386234994d8f0d3afa8b1fea
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-12-28 21:18:48 +0200

    progress update

--- app/src/main/java/com/example/placi/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/placi/AddHappyPlaceActivity.kt
@@ -7,5 +7,6 @@ class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
     }
 }
