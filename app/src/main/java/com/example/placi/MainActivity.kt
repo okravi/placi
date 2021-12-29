92db3232d431e9476065de51409e8337e24d6400
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-12-29 21:24:56 +0200

    progress update

--- app/src/main/java/com/example/placi/MainActivity.kt
+++ app/src/main/java/com/example/placi/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.placi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.placi.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
+        //setContentView(R.layout.activity_main)
 
+        val fabAddHappyPlace = binding?.fabAddHappyPlace
+        fabAddHappyPlace?.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
