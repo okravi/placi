b0b1b2ecd0b8759692927ca3915337daec420733
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-01-01 20:45:39 +0200

    progress update

--- app/src/main/java/com/example/placi/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/placi/AddHappyPlaceActivity.kt
@@ -3,8 +3,12 @@ package com.example.placi
 
 import android.Manifest
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -66,7 +70,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)).show()
             }
-            binding?.ivPlaceImage?.id ->{
+            binding?.tvAddImage?.id ->{
                 val pictureDialog = AlertDialog.Builder(this)
                 pictureDialog.setTitle("Select action")
                 val pictureDialogItems = arrayOf("Select photo from gallery",
@@ -90,11 +94,37 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
         ).withListener(object: MultiplePermissionsListener {
-            override fun onPermissionsChecked(report: MultiplePermissionsReport)
-            {/* ... */}
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?)
+            {if (report!!.areAllPermissionsGranted()){
+                Toast.makeText(this@AddHappyPlaceActivity,
+                    "Permissions granted", Toast.LENGTH_SHORT).show()
+                }
+            }
             override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken)
-            {/* ... */}
-        }).check()
+            {
+                showRationaleDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like the permissions weren't granted. That's unfortunate.")
+            .setPositiveButton("GO TO SETTINGS")
+            { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog,
+                                           _ ->
+                dialog.dismiss()
+            }.show()
     }
 
     private fun updateDateInView(){
