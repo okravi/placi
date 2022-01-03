72e0a7e1591ab7518952a8f2f47d6b7c21a92b29
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-01-03 17:51:37 +0200

    progress update

--- app/src/main/java/com/example/placi/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/placi/AddHappyPlaceActivity.kt
@@ -5,6 +5,8 @@ import android.Manifest
 import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -12,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -21,7 +24,10 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -101,6 +107,8 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     try {
                         val selectedImageBitmap = MediaStore.Images.Media
                             .getBitmap(this.contentResolver, contentURI)
+                        val saveImageToInternalStorage = saveImageToInternalStorage(selectedImageBitmap)
+                        Log.e("Saved image: ", "PAth :: $saveImageToInternalStorage")
                         binding?.ivPlaceImage?.setImageBitmap(selectedImageBitmap)
                     }catch (e: IOException){
                         e.printStackTrace()
@@ -108,8 +116,12 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                             Toast.LENGTH_SHORT).show()
                     }
                 }
-            }else if (requestCode ==  CAMERA){
+            }else if (requestCode == CAMERA){
                 val thumbnail : Bitmap = data!!.extras!!.get("data") as Bitmap
+
+                val saveImageToInternalStorage = saveImageToInternalStorage(thumbnail)
+                Log.e("Saved image: ", "PAth :: $saveImageToInternalStorage")
+
                 binding?.ivPlaceImage?.setImageBitmap(thumbnail)
             }
         }
@@ -179,8 +191,25 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         binding?.etDate?.setText(sdf.format(cal.time).toString())
     }
 
+    private fun saveImageToInternalStorage(bitmap: Bitmap): Uri{
+        val wrapper = ContextWrapper(applicationContext)
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            val stream : OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+        return Uri.parse(file.absolutePath)
+    }
+
     companion object {
         private const val GALLERY = 1
         private const val CAMERA = 2
+        private const val IMAGE_DIRECTORY = "HappyPlacesImages"
     }
 }
