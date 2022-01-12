f2f0e0cdfc144aaff7375ad63456dc1a53d0b0f6
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-01-12 21:03:49 +0200

    progress update

--- app/src/main/java/com/example/placi/activities/MapActivity.kt
+++ app/src/main/java/com/example/placi/activities/MapActivity.kt
@@ -3,10 +3,30 @@ package com.example.placi.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.placi.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
     }
 }
+
+
