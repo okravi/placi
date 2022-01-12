50354444b414a74b55b26d73086d472fe3a0c50d
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-01-12 20:41:47 +0200

    progress update

---
+++ app/src/main/java/com/example/placi/activities/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.example.placi.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.placi.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
