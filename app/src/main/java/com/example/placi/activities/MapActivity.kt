b95c4eb428e56c9795e2c64c82ada630855465b7
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-01-13 15:41:30 +0200

    progress update

--- app/src/main/java/com/example/placi/activities/MapActivity.kt
+++ app/src/main/java/com/example/placi/activities/MapActivity.kt
@@ -3,16 +3,19 @@ package com.example.placi.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.placi.R
+import com.example.placi.models.HappyPlaceModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_map.*
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
 private lateinit var mMap: GoogleMap
+private var mHappyPlaceDetails: HappyPlaceModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +24,30 @@ private lateinit var mMap: GoogleMap
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        if(intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)){
+            mHappyPlaceDetails = intent.getSerializableExtra(MainActivity.EXTRA_PLACE_DETAILS)
+                    as HappyPlaceModel?
+        }
+
+        if(mHappyPlaceDetails != null){
+            setSupportActionBar(toolbar_map)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = mHappyPlaceDetails!!.title
+
+            toolbar_map.setNavigationOnClickListener {
+                onBackPressed()
+            }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val position = LatLng(mHappyPlaceDetails!!.latitude, mHappyPlaceDetails!!.longitude)
+        mMap.addMarker(MarkerOptions().position(position).title(mHappyPlaceDetails!!.location))
 
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(position, 18f)
+        mMap.animateCamera(newLatLngZoom)
     }
 }
 
